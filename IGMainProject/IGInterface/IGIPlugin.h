e19a7f9ecdf47ce511a71c49c689d334979c3663
kupig <447265629@qq.com>
2018-12-18 16:04:14 +0800

    增加模块相关接口

--- IGMainProject/IGInterface/IGIPlugin.h
+++ IGMainProject/IGInterface/IGIPlugin.h
@@ -4,6 +4,33 @@
 #include "IGIMoudle.h"
 #include "../IGCommon/IGMap.h"
 
+#define CREATE_PLUGIN_OBJECT(pPluginManager, className)		\
+	IGIPlugin* p##className = new className(pPluginManager);\
+	pPluginManager->RegisterPlugin(p##className);
+
+#define DESTROY_PLUGIN_OBJECT(pPluginManager, className)	\
+	IGIPlugin *p##className = pPluginManager->FindPlugin(#className);\
+	pPluginManager->UnregisterPlugin(p##className);
+
+#define REGISTER_MOUDLE(pPluginManager, className)			\
+	do {													\
+		IGIMoudle *pMoudle##className = new className();	\
+		Add(#className, pMoudle##className);				\
+	} while (0)
+
+#define UNREGISTER_MOUDLE(pPluginManager, className)		\
+	do {													\
+		IGIMoudle *pMoudele##className = Remove(#className);\
+		delete pMoudele##className;							\
+	} while (0)
+
+#define EXEC_EACH_MOUDLE(FUNC_NAME)	\
+	IGIMoudle* pMoudle = First();	\
+	while (pMoudle)	{				\
+		pMoudle->FUNC_NAME();		\
+		pMoudle = Next();			\
+	}
+
 class IGIPlugin
 	: public IGIMoudle
 	, public IGMap<std::string, IGIMoudle>
@@ -12,11 +39,39 @@ public:
 	IGIPlugin() {}
 	virtual ~IGIPlugin() {}
 
-	virtual bool Awake() { return true; }	
-	virtual bool Init() { return true; }
-	virtual bool AfterInit() { return true; }
-	virtual bool Update() { return true; }
-	virtual bool Finalize() { return true; }
+	virtual bool Awake() 
+	{
+		EXEC_EACH_MOUDLE(Awake);		
+		return true; 
+	}
+
+	virtual bool Init()
+   	{
+		EXEC_EACH_MOUDLE(Init);	
+		return true;
+   	}
+
+	virtual bool AfterInit() 
+	{ 
+		EXEC_EACH_MOUDLE(AfterInit);
+		return true; 
+	}
+
+	virtual bool Update() 
+	{ 
+		EXEC_EACH_MOUDLE(Update);
+		return true; 
+	}
+
+	virtual bool Finalize()
+   	{ 
+		EXEC_EACH_MOUDLE(Finalize);
+		return true; 
+	}
+
+	virtual void InstallMoudle() = 0;
+	virtual void UninstallMoudle() = 0;
+	virtual std::string GetPluginName() = 0; 
 };
 
 #endif // IG_IPLUGIN_H
