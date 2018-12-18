d507a717be2e57ee3864509e6d2d434c2deb0f39
kupig <447265629@qq.com>
2018-12-18 16:12:32 +0800

    增加插件tutorial1

--- IGLibProject/Tutorial1/Tutorial1.cpp
+++ IGLibProject/Tutorial1/Tutorial1.cpp
@@ -1,12 +1,45 @@
 #include "Tutorial1.h"
 #include <stdio.h>
+#include "../../IGMainProject/IGCommon/IGPlatform.h"
+#include "TutorialMoudle1.h"
 
-extern "C" void LibPluginStart()
+extern "C" void LibPluginStart(IGIPluginManager *pPluginManager)
 {
-	printf("hello, this is tutorial.so start.\n");
+	printf("\ncreate and register Tutorial1 in pPluginManager.\n");
+	CREATE_PLUGIN_OBJECT(pPluginManager, Tutorial1);
 }
 
-extern "C" void LibPluginStop()
+extern "C" void LibPluginStop(IGIPluginManager *pPluginManager)
 {
-	printf("hello, this is tutorial.so end.\n");
+	printf("\nstop and register Tutorial1 in pPluginManager.\n");
+	DESTROY_PLUGIN_OBJECT(pPluginManager, Tutorial1);
+}
+
+Tutorial1::Tutorial1(IGIPluginManager* p)
+{
+	pPluginManager = (void *)p;
+}
+
+Tutorial1::~Tutorial1()
+{
+}
+
+void
+Tutorial1::InstallMoudle()
+{
+	PRINT_FUNC();
+	REGISTER_MOUDLE(pPluginManager, TutorialMoudle1);
+}
+
+void
+Tutorial1::UninstallMoudle()
+{
+	PRINT_FUNC();
+	UNREGISTER_MOUDLE(pPluginManager, TutorialMoudle1);
+}
+
+std::string
+Tutorial1::GetPluginName()
+{
+	return PLUGIN_NAME(Tutorial1);
 }
