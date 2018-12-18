31feed30be41829b041f664b2cb6ae2419bba934
kupig <447265629@qq.com>
2018-12-18 16:13:01 +0800

    Update Tutorial1.h

--- IGLibProject/Tutorial1/Tutorial1.h
+++ IGLibProject/Tutorial1/Tutorial1.h
@@ -2,7 +2,18 @@
 #define TUTORIAL1_H
 
 #include "../../IGMainProject/IGInterface/IGIPlugin.h"
-#include "../../IGMainProject/IGPluginLoader/IGPluginManager.h"
+#include "../../IGMainProject/IGInterface/IGIPluginManager.h"
 
+class Tutorial1: public IGIPlugin
+{
+public:
+	Tutorial1(IGIPluginManager *p);
+	virtual ~Tutorial1();
+
+	virtual void InstallMoudle();
+	virtual void UninstallMoudle();
+
+	std::string GetPluginName();
+};
 
 #endif //TUTORIAL1_H
