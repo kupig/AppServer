56f1b77cfff72c17a66f7f04c72a07ef4dfb3ea4
zhangyan <447265629@qq.com>
2019-02-20 22:09:45 +0800

    add msg head

--- IGLibProject/NetPlugin/IGIMsgHead.h
+++ IGLibProject/NetPlugin/IGIMsgHead.h
@@ -11,7 +11,7 @@ public:
 		IG_HEAD_LENGTH = 6,
 	};
 
-	virtual int EnCode(const char *data) = 0;
+	virtual int EnCode(char *data) = 0;
 	virtual int DeCode(const char *data) = 0;		
 	
 	virtual int GetMsgID() = 0;
@@ -24,14 +24,37 @@ public:
 class IGCMsgHead : public IGIMsgHead
 {
 public:
-	IGCMsgHead() {}
+	IGCMsgHead() : mSize(0), mMsgID(0) {}
 	virtual ~IGCMsgHead() {}	
 
-	virtual int EnCode(const char *data)
+	virtual int EnCode(char *data)
 	{
-		int nOffset = 0;
+		int offset = 0;
 		
+		// msgID = 2Byte		
+		uint16_t msgId = htons(mMsgID);	
+		memcpy(data + offset, (void *)&mMsgID, sizeof(mMsgID));
+		offset += sizeof(mMsgID);
+
+		// msg size = 4Byte
+		uint32_t packSize = mSize + IG_HEAD_LENGTH;
+		uint32_t msgSize = htobe32(packSize);
+		memcpy(data + offset, (void *)&msgSize, sizeof(msgSize));
+		offset += sizeof(msgSize);	
+
+		if (offset != IG_HEAD_LENGTH)
+		{
+			assert(0);	
+		}
 	}
+
+	virtual int DeCode(const char *data)
+	{
+	}
+
+private:
+	uint32_t mSize;
+	uint16_t mMsgID;
 };
 #pragma pack(pop)
 
